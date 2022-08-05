6d933defd15839248bdfafb755fdcede88e47934
ivorobioff <igor.vorobioff@gmail.com>
2022-08-05 18:48:20 +0300

    some updates

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  useContainer(app, { fallbackOnErrors: true });
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   
   await app.listen(3000);
 }
