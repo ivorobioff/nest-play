49434592c542cfd84f034a5609daec91bad4bb09
ivorobioff <igor.vorobioff@gmail.com>
2022-08-19 23:47:59 +0300

    some updates

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { AuthResult } from './auth.result';
 import { AuthService } from './auth.service';
 import { CredentialsPayload } from './credentials.payload';
+import { Open } from './open.decorator';
 
 @Controller('auth')
 export class AuthController {
 
     constructor(private authService: AuthService) {}
     
+    @Open()
     @Post("login")
     async login(@Body() payload: CredentialsPayload): Promise<AuthResult> {
         let result = await this.authService.login(payload);
